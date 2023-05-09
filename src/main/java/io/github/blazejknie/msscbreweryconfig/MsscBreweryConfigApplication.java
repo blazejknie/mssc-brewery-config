eae24f1aac2645e24190c5a6aa145e71f94aa1b5
Blazej_Knie <bjke@gft.com>
2023-05-09 17:10:54 +0200

    initial project add

---
+++ src/main/java/io/github/blazejknie/msscbreweryconfig/MsscBreweryConfigApplication.java
@@ -0,0 +1,13 @@
+package io.github.blazejknie.msscbreweryconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryConfigApplication.class, args);
+    }
+
+}
